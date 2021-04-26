249d01dd6887bdf4ce63b1855a72db755a14676f
mmarat01 <mijaelms@protonmail.com>
2021-04-26 18:26:32 -0400

    re-structure folders/files, tweak express app setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./db");
+const db = require("./server/db");
 const cors = require("cors");
 const https = require("https");
 const path = require("path");
@@ -8,7 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
+app.use(express.static("client"));
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+const apiPort = process.evn.PORT || 3000;
+app.listen(apiPort, () => console.log(`Server is running on port ${apiPort}`));
