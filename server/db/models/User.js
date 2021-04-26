6286d0438d4afb23eb2b77e6e9ec6e1cfac69a58
mmarat01 <mijaelms@protonmail.com>
2021-04-26 14:48:23 -0400

    model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,1 +1,19 @@
 // user
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  holdings: [
+    { name: String, ticker: String, amount: Number, purchase_time: Date },
+  ],
+});
+
+module.exports = mongoose.model("users", User);
