fb57ffcff012de32c7e2908f81699afd444be5ea
Jimmy Maslen <james.maslen@tufts.edu>
2021-05-04 10:52:41 -0400

    add current crypto data to profile page

--- client/scripts/buildProfile.js
+++ client/scripts/buildProfile.js
@@ -1,3 +1,5 @@
+// get user info
+// get info on currencies
 
 function writeHoldings() {
   const token = getCookie("token");
@@ -8,38 +10,53 @@ function writeHoldings() {
     })
       .then((response) => response.json())
       .then((dataObj) => {
-        console.log(dataObj.data);
-        userInfo = dataObj.data;
-        let greet = document.querySelector("#greeting");
-        greet.innerHTML = `Hello ${userInfo.username}`;
-        let holdings_list = document.querySelector("#holdings-list");
-        holdings_list.innerHTML=""
-        for (let i = 0; i < userInfo.holdings.length; i++) {
-          holding = userInfo.holdings[i]
-          holdings_list.innerHTML +=
+        fetch('/api/crypto/all')
+        .then((crypto_response) => crypto_response.json())
+        .then((crypto_dataObj) => {
+          userInfo = dataObj.data;
+          let greet = document.querySelector("#greeting");
+          greet.innerHTML = `Hello ${userInfo.username}`;
+          let holdings_list = document.querySelector("#holdings-list");
+          holdings_list.innerHTML=""
+          for (let i = 0; i < userInfo.holdings.length; i++) {
+            holding = userInfo.holdings[i]
+            // find holding in crypto array
+            let currentCrypto
+            for (let crypto of crypto_dataObj.data) {
+              if (crypto.symbol == holding.ticker) {
+                currentCrypto = crypto
+                break
+              }
+            }
+            let percentChange = ((Number(currentCrypto.price) - holding.purchase_price) / (holding.purchase_price)) * 100
+
+            holdings_list.innerHTML +=
+              `
+            <tr> 
+              <th scope="row">${i + 1}</th>
+              <td>${holding.name}</td>
+              <td>${holding.ticker}</td>
+              <td>$${holding.purchase_price.toFixed(2)}</td>
+              <td>$${Number(currentCrypto.price).toFixed(2)}</td>
+              <td style="color:${percentChange > 0? "green": "red"}">${Number(percentChange).toFixed(2)}%</td>
+              <td>
+              <button 
+                class="btn btn-primary col-12" 
+                type="submit" 
+                data-toggle="modal" 
+                data-target="#exampleModal" 
+                data-name="${holding.name}"
+                data-symbol="${holding.ticker}"
+                data-price="${holding.purchase_price}">
+                  Sell
+              </button>
+              </td>
+              
+            </tr>
             `
-          <tr> 
-            <th scope="row">${i + 1}</th>
-            <td>${holding.name}</td>
-            <td>${holding.ticker}</td>
-            <td>${holding.purchase_price.toFixed(2)}</td>
-            <td>
-            <button 
-              class="btn btn-primary col-12" 
-              type="submit" 
-              data-toggle="modal" 
-              data-target="#exampleModal" 
-              data-name="${holding.name}"
-              data-symbol="${holding.ticker}"
-              data-price="${holding.purchase_price}">
-                Sell
-            </button>
-            </td>
-            
-          </tr>
-          `
-        }
-      });
+          }
+        });
+        })
   }
 }
 
